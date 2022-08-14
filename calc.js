4a24d45bdf816d42a3dd87b8069a4218ee7fc5dc
Tyler Mayes <timmymayes@gmail.com>
2022-08-13 17:11:49 -0700

    Implement basic functions add, subtract, mutiply, divide

---
+++ calc.js
@@ -0,0 +1,18 @@
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function divide(num1, num2) {
+  if (num2 === 0) {
+    return "ERROR";
+  }
+  return num1 / num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}

38b8036327fc28cb5278f3ecd8998daeb006d38a
Tyler Mayes <timmymayes@gmail.com>
2022-08-13 17:49:25 -0700

    Give each button an ID and create logic to parse pressed buttons

--- calc.js
+++ calc.js
@@ -1,3 +1,6 @@
+const MAX_HISTORY_LENGTH = 35;
+const MAX_BUFFER_LENGTH = 20;
+
 function sum(num1, num2) {
   return num1 + num2;
 }
@@ -34,4 +37,21 @@ function operate(num1, num2, operator) {
       break;
   }
   return result;
+}
+
+const buttons = document.querySelectorAll(".button");
+buttons.forEach((btn) => btn.addEventListener("click", executeClick));
+
+function executeClick(e) {
+  let operator = "";
+  let operand = 0;
+  if (e.target.id === "=") {
+    console.log("run operate function");
+  } else if (Number.isNaN(parseInt(e.target.id))) {
+    operator = e.target.id;
+  } else {
+    operand = e.target.id;
+  }
+  console.log("Current Operation: " + operator);
+  console.log("Current Operand: " + operand);
 }

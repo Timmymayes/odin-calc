a9fa7e926ebe35f1f16638ca1eea2d5ef1f17e22
Tyler Mayes <timmymayes@gmail.com>
2022-08-13 17:17:31 -0700

    Implement operate command.
    Accepts two numbers and an operator outputs result of operation.

--- calc.js
+++ calc.js
@@ -16,3 +16,22 @@ function divide(num1, num2) {
 function multiply(num1, num2) {
   return num1 * num2;
 }
+
+function operate(num1, num2, operator) {
+  let result;
+  switch (operator) {
+    case "+":
+      result = sum(num1, num2);
+      break;
+    case "-":
+      result = subtract(num1, num2);
+      break;
+    case "/":
+      result = divide(num1, num2);
+      break;
+    case "*":
+      result = multiply(num1, num2);
+      break;
+  }
+  return result;
+}

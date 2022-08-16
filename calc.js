fe37a9e95d3d96c0285b01a9f9f0fe3ebed5ff04
Tyler Mayes <timmymayes@gmail.com>
2022-08-15 21:30:14 -0700

    Fix chained calculations using operators to calculate pairs.

--- calc.js
+++ calc.js
@@ -1,5 +1,5 @@
 const MAX_HISTORY_LENGTH = 35;
-const MAX_BUFFER_LENGTH = 20;
+const MAX_BUFFER_LENGTH = 18;
 
 function sum(num1, num2) {
   return parseInt(num1) + parseInt(num2);
@@ -40,73 +40,114 @@ function operate(calculation) {
 }
 
 let args = {};
-let needsCleared = false;
+
 const buffer = document.getElementById("buffer-text");
 const history = document.getElementById("history-text");
 const buttons = document.querySelectorAll(".button");
+
 buttons.forEach((btn) => btn.addEventListener("click", executeClick));
 
 function executeClick(e) {
   let calculation;
+
   if (e.target.id === "=") {
+    // Equals case
     if (
       args.operator != undefined &&
       args.prevOperand != undefined &&
       args.operand != undefined
     ) {
-      clearHistory();
-      appendHistory(args.prevOperand + args.operator + args.operand);
-      clearBuffer();
-      calculation = operate(args);
-      appendBuffer(calculation);
-      needsCleared = true;
+      let solution = calculate(args);
       args = {};
-      args.prevOperand = calculation;
+      args.prevOperand = solution;
+      clearButtons();
     }
   } else if (e.target.id === "CLR") {
-    args.operand = undefined;
-    args.operator = undefined;
+    // Clear
     clearBuffer();
-    if (args.prevOperand) {
-      appendBuffer(args.prevOperand);
-    }
+    clearButtons();
+    args = {};
   } else if (e.target.id === "AC") {
-    console.log("made it into all clear");
+    // all clear
     clearHistory();
     clearBuffer();
-    args.operand = undefined;
-    args.operator = undefined;
-    args.prevOperand = undefined;
+    clearButtons();
+    args = {};
   } else if (Number.isNaN(parseInt(e.target.id))) {
-    console.log("made it into operators");
-    if (args.operand != undefined) {
-      args.prevOperand = args.operand;
+    // operators
+
+    // if we have an ope
+    if (args.operand != undefined && args.prevOperand != undefined) {
+      let solution = calculate(args);
+      args = {};
+      args.prevOperand = solution;
+      clearButtons();
+      args.isCalculating = true;
     }
-    if (args.operator === undefined) {
-      appendBuffer(e.target.id);
-      args.operator = e.target.id;
+
+    // first calculation in a chain
+    if (!args.isCalculating) {
+      args.prevOperand = args.operand;
       args.operand = undefined;
-      needsCleared = false;
+      args.isCalculating;
+    }
+
+    // set operator & highlighting
+    args.prevOperator = args.operator;
+    args.operator = e.target.id;
+    clearButtons();
+    e.target.style.background = "grey";
+
+    // clear buttons if operator is pressed first.
+    if (args.operand === undefined && args.prevOperand === undefined) {
+      clearButtons();
+      clearBuffer();
     }
   } else {
-    console.log("made it into numbers");
-    if (needsCleared) {
+    //Numbers
+
+    //build the operand
+
+    if (args.isCalculating) {
       clearBuffer();
-      calculation = null;
+      args.isCalculating = false;
     }
+
     if (args.operand === undefined) {
       args.operand = e.target.id;
+      if (buffer.textContent != "") {
+        clearBuffer();
+      }
       appendBuffer(e.target.id);
     } else {
-      if (args.operand.length < MAX_BUFFER_LENGTH) {
-        appendBuffer(e.target.id);
-        args.operand += parseInt(e.target.id);
-      }
+      appendBuffer(e.target.id);
+      args.operand += parseInt(e.target.id);
     }
   }
+
   console.log("Current Operation: " + args.operator);
   console.log("Current Operand: " + args.operand);
   console.log("Previous Operand: " + args.prevOperand);
+}
+
+function calculate(equation) {
+  console.log("Calculating...");
+  let answer;
+  clearHistory();
+  appendHistory(equation.prevOperand + equation.operator + args.operand);
+  clearBuffer();
+  answer = operate(equation);
+  if (answer.length > MAX_BUFFER_LENGTH) {
+    if (!answer.includes(".")) {
+      answer = "OVERFLOW";
+    } else {
+      answer = parseFloat(answer).toFixed(18 - answer.indexOf("."));
+      console.log("Answer Rounded");
+    }
+  }
+  appendBuffer(answer);
+
+  return answer;
 }
 
 function appendBuffer(textToDisplay) {
@@ -123,4 +164,11 @@ function clearHistory() {
 
 function appendHistory(textToAppend) {
   history.textContent += textToAppend;
+}
+
+function clearButtons() {
+  document.getElementById("-").style.background = "lightgrey";
+  document.getElementById("*").style.background = "lightgrey";
+  document.getElementById("/").style.background = "lightgrey";
+  document.getElementById("+").style.background = "lightgrey";
 }
